107dc824c5d580a338c03a1460777298834a280a
Shenal Hettiarachchi <IT25102784@my.sliit.lk>
2025-10-04 23:41:47 +0530

    Add files via upload

--- PM_assignment.c
+++ PM_assignment.c
@@ -119,9 +119,8 @@ int win_check(char current, int N, char board[N][N]) {
     
     return 0;
 }
-// Function to create random moves
-void computer_move(int N,char board[N][N],int*row,int *col){
-	//int row,col;
+// Function to create random moves when mode=1
+void computer_move_mode1(int N,char board[N][N],int*row,int *col){
 	do{
 		*row = rand()%N;
 		*col = rand()%N;
@@ -151,7 +150,7 @@ void save_win(int i,char current,int T,int N){
 	}
 	else{
 		if(T==N*N){
-			fprintf(ptr,"The Game is a Draw");
+			fprintf(ptr,"The game is a Draw");
                 fclose(ptr);
 		}
 		else{
@@ -159,6 +158,16 @@ void save_win(int i,char current,int T,int N){
 		fclose(ptr);
 		}
 	}
+}
+// function for computer to generate and store a move when mode ==3
+void computer_move2(int N,char board[N][N],int*row,int *col,char current){
+	do{
+		*row = rand()%N;
+		*col = rand()%N;
+	}while(board[*row][*col]!=' ');
+	board[*row][*col]=current;
+	printf("the computer has played the move row %d and colomn %d\n",*row,*col);
+	print_board(N,board);
 }
 
 
@@ -170,7 +179,8 @@ int main(){
 	 if (N<3 || N>10){
 	       printf("Invalid input!\n");
          return 1;
-	}       
+	}
+
 	int mode;
 	printf("Select the mode\n");
 	printf("Mode 1 ==> User vs computer\n");
@@ -178,16 +188,42 @@ int main(){
         printf("Mode 3 ==> Mulit-Player (3 Users ) \n");
 	printf("Enter the game mode: ");
 	scanf("%d",&mode);
+	char player_type[3];
+	if(mode==3){
+		printf("Enter the player type\n");
+		printf("H ==> Human, C ==> computer\n");
+		int H_cnt;		// count the human players
+		do{
+		for(int i=0;i<3;i++){
+			printf("Player type for plyer number %d:",i+1);
+		        scanf(" %c",&player_type[i]);
+				    }
+		for(int j=0;j<3;j++){
+			if( player_type[j]=='H'||player_type[j]=='h'){
+				H_cnt++;
+			}
+			else {
+				continue;
+			}
+		}
+		if(H_cnt<1){
+			printf("At least there must be one human player to play multi-player mode\n");
+			printf("plase enter again");
+		}
+		}while(H_cnt<1); 	// since the human count should be at least one 
+
+		printf("\n");
+	}
+
 	initialize_game_board(N,board);
 	print_board(N,board);
 	int turn=0;
 	char current_player;
-	//int i ;
+	if(mode==2||mode==1){
 	for (int i=0;i<N*N; i++){
 	 int row,col;
 	 turn;
-         int winchk;
-	     char player[3]={'X','O','Z'};
+	     char player[2]={'X','O'};
 	      current_player = player[turn % mode];
 	           input_validation(current_player,&row,&col,N,board);
 		   board[row][col] = current_player;
@@ -202,18 +238,74 @@ int main(){
 		    srand(time(NULL));
 		    if (mode==1){
 			    int temp_row,temp_col;
-		    computer_move(N,board,&temp_row,&temp_col);
+		    computer_move_mode1(N,board,&temp_row,&temp_col);
 		    save_move(i+1,temp_row,temp_col,'O');
 		    i++;
+		    if(win_check('O',N,board)==1){
+                        printf("The computer has won the game!");
+                        save_win(i,'O',turn,N);
+
+                          return 0;
+                       }
 		    }
 		   turn++;
 
 	}
-	printf("The game is a DRAW!\n");
-       FILE *F_ptr = fopen("game_history.txt", "a");// saves the draw in log file
+	 printf("The game is a DRAW!\n");
+         FILE *F_ptr = fopen("game_history.txt", "a");// saves the draw in log file
          if (F_ptr != NULL) {
                   fprintf(F_ptr, "The game ended in a DRAW!\n");
                                 fclose(F_ptr);
               }
+	}
+	//If the mode is 3
+	if(mode==3){
+		int turn=0;
+		char player[3]={'X','O','Z'};
+		int i;
+		for (i=0;i<N*N;i++){
+			int row,col;
+			char current = player[turn%3];
+			char type = player_type[turn%3];
+			if (type=='H'||type =='h'){
+				input_validation(current,&row,&col,N,board);
+				board[row][col]=current;
+				save_move(i,row,col,current);
+				if(win_check(current,N,board)==1){
+					printf("player %c has won the game!",current);
+					save_win(i,current,turn,N);
+					return 0;
+				}
+			}
+			if (type=='C'|| type=='c'){
+				srand(time(NULL));
+				computer_move2(N,board,&row,&col,current);
+				save_move(i,row,col,current);
+				if(win_check(current,N,board)==1){
+					printf("The computer has won the game!");
+					FILE *F_ptr = fopen("game_history.txt", "a");// saves the draw in log file
+               		    		if (F_ptr != NULL) {
+                  				fprintf(F_ptr, "The computer has won the game!\n");
+                                		fclose(F_ptr);
+                                	 		  }
+					else{
+						printf("Error opening the file ");
+					}
+					return 0;
+				}
+			}
+			turn++;
+		}
+		 print_board(N,board);
+		printf("The game is a DRAW!");
+		FILE *F_ptr = fopen("game_history.txt", "a");// saves the draw in log file
+         	if (F_ptr != NULL) {
+                  fprintf(F_ptr, "The game ended in a DRAW!\n");
+                                fclose(F_ptr);
+             			 }
+		else{
+			printf("Error opening the file ");
+		}
+	}
         return 0;
- }
+    }
